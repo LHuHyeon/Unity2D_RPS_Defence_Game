b904cc618902e68a7dfce6e10bdf05de9d9cf681
LHuHyeon <minfkffn@gmail.com>
2023-11-20 21:14:14 +0900

    FEAT: 로비 Scene UI 제작 중

--- UI/Scene/UI_LobyScene.cs
+++ UI/Scene/UI_LobyScene.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UI_LobyPopup : MonoBehaviour
+public class UI_LobyScene : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
